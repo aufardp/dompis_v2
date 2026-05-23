8ab63c37852bc1a70423380e70b0abd169a2d2f2
aufardp <aufardanu@gmail.com>
2026-05-23 12:41:47 +0700

    fix: increase max memory dompis-server from 8G to 16G

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       instances: Number(process.env.WEB_INSTANCES || 1),
       exec_mode:
         Number(process.env.WEB_INSTANCES || 1) > 1 ? 'cluster' : 'fork',
-      max_memory_restart: '8G',
+      max_memory_restart: '16G',
       env: {
         NODE_ENV: 'production',
         HOSTNAME: '0.0.0.0',
